cd5deee187b22528baca1ffa20a4d99a1f19b4ca
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2024-02-01 21:53:39 -0600

    chat command testing

--- src/main/java/com/AutoMapArt/AutoMapArt.java
+++ src/main/java/com/AutoMapArt/AutoMapArt.java
@@ -1,9 +1,19 @@
 package com.AutoMapArt;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.BlockPosArgumentType;
+import net.minecraft.text.Text;
+
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+
+
 
 public class AutoMapArt implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -13,5 +23,14 @@ public class AutoMapArt implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("foo")
+		.requires(source -> source.isExecutedByPlayer())
+		.then(argument("test", IntegerArgumentType.integer()))
+		.executes(context -> {
+			context.getSource().sendFeedback(() -> Text.literal("Foo"), false);
+
+			
+			return 1;
+		})));
 	}
 }
